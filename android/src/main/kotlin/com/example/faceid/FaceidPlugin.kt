75058b6c41d7018ccf2e0da1d4911563a9552f27
jesus.navarro <jesus.navarro@grupocsi.com>
2025-08-18 07:49:14 -0600

    Correcciones 14

--- android/src/main/kotlin/com/example/faceid/FaceidPlugin.kt
+++ android/src/main/kotlin/com/example/faceid/FaceidPlugin.kt
@@ -7,9 +7,10 @@ import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import android.graphics.BitmapFactory
 import FaceIDLibrary
+import androidx.annotation.NonNull
 
 
-class FaceidPlugin: FlutterPlugin {
+/* class FaceidPlugin: FlutterPlugin {
   private val CHANNEL = "com.lionintel.faceid/faceid"
   
   private lateinit var faceIdLib: FaceIDLibrary
@@ -31,4 +32,39 @@ class FaceidPlugin: FlutterPlugin {
       }
     }
   }
+} */
+
+class FaceidPlugin : FlutterPlugin, MethodCallHandler {
+
+    private val CHANNEL = "com.lionintel.faceid/faceid"
+    private lateinit var channel: MethodChannel
+    private lateinit var faceIdLib: FaceIDLibrary
+
+    override fun onAttachedToEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel = MethodChannel(binding.binaryMessenger, CHANNEL)
+        channel.setMethodCallHandler(this)
+
+        // Inicializas tu librería con el contexto
+        faceIdLib = FaceIDLibrary(binding.applicationContext)
+    }
+
+    override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+        when (call.method) {
+            "getVectors" -> {
+                val byteArray = call.argument<ByteArray>("image")
+                if (byteArray == null) {
+                    result.error("INVALID_ARGUMENT", "Image data is null", null)
+                    return
+                }
+                val bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.size)
+                val response = faceIdLib.getVectors(bitmap)
+                result.success(response)
+            }
+            else -> result.notImplemented()
+        }
+    }
+
+    override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+        channel.setMethodCallHandler(null)
+    }
 }
