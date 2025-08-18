0914db91b38848d36d388ad2cf74118ff2a30a88
jesus.navarro <jesus.navarro@grupocsi.com>
2025-08-17 22:49:11 -0600

    Correcciones 10

--- android/src/main/kotlin/com/example/faceid/FaceidPlugin.kt
+++ android/src/main/kotlin/com/example/faceid/FaceidPlugin.kt
@@ -5,6 +5,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import android.graphics.BitmapFactory
 import com.lionintel.faceidlibrary.FaceIDLibrary
 
 
